720094dfb5684feafd7fcb23e4cd1ad75f7ce48d
eSklarzyk <emanuela@post.com>
2017-07-13 16:32:36 -0400

    .

--- Scripts/Core/_reference.ts
+++ Scripts/Core/_reference.ts
@@ -2,7 +2,6 @@
 /// <reference path="../objects/asset.ts"/>
 /// <reference path="../objects/button.ts"/>
 /// <reference path="../objects/label.ts"/>
-/// <reference path="../objects/scene.ts"/>
 /// <reference path="../objects/vector2.ts"/>
 /// <reference path="../objects/gameObject.ts"/>
 /// <reference path="../objects/background.ts"/>
