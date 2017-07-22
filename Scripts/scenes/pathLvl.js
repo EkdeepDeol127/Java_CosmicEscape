71b551bfea82f40edada00c4b68efc86bcda9507
eSklarzyk <emanuela@post.com>
2017-07-22 10:11:09 -0400

    tune ups for merge

--- Scripts/scenes/pathLvl.js
+++ Scripts/scenes/pathLvl.js
@@ -12,15 +12,63 @@ var scenes;
 (function (scenes) {
     var pathLevel = (function (_super) {
         __extends(pathLevel, _super);
-        //private variables
-        //creates an instance of pathLvl
+        //creates an instance on pathLevel
         function pathLevel() {
             return _super.call(this) || this;
         }
+        pathLevel.prototype._updateScoreBoard = function () {
+            this._livesLabel.text = "Lives: " + core.lives;
+            this._scoreLabel.text = "Score: " + core.score;
+        };
         pathLevel.prototype.Start = function () {
+            //adds background
+            this._backgr = new objects.Background("pathLevel");
+            this.addChild(this._backgr);
+            this._player = new objects.Player("player");
+            this.addChild(this._player);
+            this._bullet = new objects.Bullet("BUllet");
+            this.addChild(this._bullet);
+            this._enemyBullet = new objects.EnemyBullet("BUllet");
+            this.addChild(this._enemyBullet);
+            this._enemyShip = new objects.EnemyShip("player");
+            this.addChild(this._enemyShip);
+            this._asteroid = new Array();
+            for (var count = 0; count < 4; count++) {
+                this._asteroid.push(new objects.Asteroid("asteroid"));
+                this.addChild(this._asteroid[count]);
+            }
+            this._collision = new managers.Collision();
+            this._scoreLabel = new objects.Label("Score: " + core.score, "40px", "Dock51", "#FFFF00", 350, 5, false);
+            this.addChild(this._scoreLabel);
+            this._livesLabel = new objects.Label("Lives: " + core.lives, "40px", "Dock51", "#FFFF00", 10, 5, false);
+            this.addChild(this._livesLabel);
             core.stage.addChild(this);
         };
         pathLevel.prototype.Update = function () {
+            var _this = this;
+            this._galaxy.update();
+            this._player.giveData(core.stage.mouseX, core.stage.mouseY);
+            this._player.update();
+            this._bullet.giveData(core.stage.mouseX, core.stage.mouseY, this._player.x, this._player.y);
+            this._bullet.update();
+            this._enemyShip.giveData(this._player.x, this._player.y);
+            this._enemyShip.update();
+            this._enemyBullet.giveData(this._player.x, this._player.y, this._enemyShip.x, this._enemyShip.y, this._enemyShip.inRange);
+            this._enemyBullet.update();
+            this._collision.check(this._player, this._enemyShip);
+            this._collision.check(this._player, this._enemyShip);
+            //asteroids update
+            this._asteroid.forEach(function (asteroid) {
+                asteroid.giveData(_this._player.x, _this._player.y);
+                _this._collision.check(_this._player, asteroid);
+                asteroid.update();
+            });
+            this._updateScoreBoard();
+            if (core.lives < 1) {
+                core.scene = config.Scene.OVER;
+                core.changeScene();
+                core.lives = 5;
+            }
         };
         return pathLevel;
     }(objects.Scene));
