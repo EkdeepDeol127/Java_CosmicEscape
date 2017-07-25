83bd55bf72f9d147fdeca0aca7531f1514a7d740
eSklarzyk <emanuela@post.com>
2017-07-25 12:15:22 -0400

    main page buttons

--- Scripts/scenes/mainMenu.js
+++ Scripts/scenes/mainMenu.js
@@ -25,17 +25,17 @@ var scenes;
             this._backgr = new objects.Background("mainPage");
             this.addChild(this._backgr);
             //add start  button
-            this._startButton = new objects.Button("playButton", 380, 300, true);
+            this._startButton = new objects.Button("playButton", 400, 350, true);
             this.addChild(this._startButton);
             //startbutton event listener
             this._startButton.on("click", this._startButtonClick, this);
             //add tutorial button
-            this._tutorialButton = new objects.Button("tutButton", 380, 350, true);
+            this._tutorialButton = new objects.Button("tutorialButton", 400, 400, true);
             this.addChild(this._tutorialButton);
             //tutButton listener
             this._tutorialButton.on("click", this._tutButtonClick, this);
             //add development buttin
-            this._pathButton = new objects.Button("againButton", 380, 400, true);
+            this._pathButton = new objects.Button("againButton", 150, 450, true);
             this.addChild(this._pathButton);
             //path button listener
             this._pathButton.on("click", this._pathButtonClick, this);
