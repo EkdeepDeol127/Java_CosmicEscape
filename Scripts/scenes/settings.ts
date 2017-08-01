0b93f8a9bab2c583162dc6c083c55a1e7871b066
eSklarzyk <emanuela@post.com>
2017-08-01 11:36:34 -0400

    .

--- Scripts/scenes/settings.ts
+++ Scripts/scenes/settings.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module scenes {
     export class Settings extends objects.Scene {
 
@@ -47,38 +46,5 @@ module scenes {
                     this.SCheck = false;
                 }
         }
-=======
-module scenes{
-    export class Settings extends objects.Scene{
-
-
-//private instance variables
-private _backButt: objects.Button;
-private _backgr: objects.Background;
-
-//create an instance of credits
-constructor(){
-    super();
-}
-
-public Start():void{
-//add background 
-this._backgr = new objects.Background("galaxy");
-this.addChild(this._backgr);
-//add button
-this._backButt = new objects.Button("backButton",370,200,true);
-this.addChild(this._backButt);
-this._backButt.on("click", this._buttClick,this);
-
-core.stage.addChild(this);
-}
-
-private _buttClick(event:createjs.MouseEvent):void{
-    //switch scene
-core.scene = config.Scene.MENU;
-core.changeScene();
-}
-
->>>>>>> featureEdits
     }
 }
