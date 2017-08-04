ee6a9f457948ca22fd1ae131ab790be7e9ec6458
Ekdeep <ekdeep.deol@hotmail.com>
2017-08-03 20:43:18 -0400

    updated credits

--- Scripts/scenes/credits.ts
+++ Scripts/scenes/credits.ts
@@ -4,7 +4,8 @@ module scenes {
         //private instance variables
         private _backButt: objects.Button;
         private _backgr: objects.Background;
-
+        private _text1: objects.Label;
+        private _text2: objects.Label;
         private _theSound: createjs.AbstractSoundInstance;
 
         //create an instance of credits
@@ -19,8 +20,13 @@ module scenes {
             }
 
             //add background 
-            this._backgr = new objects.Background("galaxy");
+            this._backgr = new objects.Background("mainPage");
             this.addChild(this._backgr);
+            
+            this._text1 = new objects.Label("Emanuela Sklarzyk............Producer", "25px", "monospace", "#FFFF00", 10, 400, false);
+            this.addChild(this._text1);
+            this._text2 = new objects.Label("Ekdeep Deol............Software Engineer", "25px", "monospace", "#FFFF00", 10, 450, false);
+            this.addChild(this._text2);
             //add button
             this._backButt = new objects.Button("backButton", 370, 500, true);
             this.addChild(this._backButt);
